5b37bdc6b9936dcdd83a8dce8c5fabee27b5ff40
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-06-20 11:06:49 -0700

    refactor & fix Ev3rstorm.blast_bazooka_if_touched(...) method

--- official_models/ev3/home/ev3rstorm/main.py
+++ official_models/ev3/home/ev3rstorm/main.py
@@ -103,21 +103,18 @@ class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
 
     def __init__(
             self,
-            left_motor_port: Port = Port.B,
-            right_motor_port: Port = Port.C,
-            shooting_motor_port: Port = Port.A,
-            touch_sensor_port: Port = Port.S1,
-            color_sensor_port: Port = Port.S3,
-            ir_sensor_port: Port = Port.S4,
-            ir_beacon_channel: int = 1):
+            left_leg_motor_port: Port = Port.B, right_leg_motor_port: Port = Port.C,
+            bazooka_blast_motor_port: Port = Port.A,
+            touch_sensor_port: Port = Port.S1, color_sensor_port: Port = Port.S3,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
         IRBeaconDriverMixin.__init__(
             self,
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
-            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            left_motor_port=left_leg_motor_port, right_motor_port=right_leg_motor_port,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
         
-        self.shooting_motor = Motor(port=shooting_motor_port,
-                                    positive_direction=Direction.CLOCKWISE)
+        self.bazooka_blast_motor = Motor(port=bazooka_blast_motor_port,
+                                         positive_direction=Direction.CLOCKWISE)
 
         self.touch_sensor = TouchSensor(touch_sensor_port)
         self.color_sensor = ColorSensor(color_sensor_port)
@@ -131,18 +128,18 @@ class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
             if self.color_sensor.ambient() < 15:
                 self.speaker.play_file(file=SoundFile.UP)
 
-                self.shooting_motor.run_angle(
+                self.bazooka_blast_motor.run_angle(
                     speed=2 * MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,   # shoot quickly in half a second
-                    rotation_angle=MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
+                    rotation_angle=-MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
                     then=Stop.HOLD,
                     wait=True)
 
             else:
                 self.speaker.play_file(file=SoundFile.DOWN)
 
-                self.shooting_motor.run_angle(
+                self.bazooka_blast_motor.run_angle(
                     speed=2 * MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,   # shoot quickly in half a second
-                    rotation_angle=-MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
+                    rotation_angle=MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
                     then=Stop.HOLD,
                     wait=True)
 
