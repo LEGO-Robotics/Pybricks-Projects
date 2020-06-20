247742f48e382c13168d5f7861082e8b1bc7d4dd
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-06-20 10:14:19 -0700

    move IRBeaconDriverMixin into main.py for ease of showing tutorial code on the PyBricks website

--- official_models/ev3/home/ev3rstorm/main.py
+++ official_models/ev3/home/ev3rstorm/main.py
@@ -12,12 +12,88 @@ https://www.lego.com/en-us/themes/mindstorms/buildarobot
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor
+from pybricks.media.ev3dev import ImageFile, SoundFile
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import ImageFile
-from pybricks.parameters import Direction, Port, Stop
+from pybricks.parameters import Button, Direction, Port, Stop
 
-from drive_util import IRBeaconDriverMixin
+
+# this reusable mixin provides the capability of driving a robot with a Driving Base by the IR beacon
+class IRBeaconDriverMixin:
+    def __init__(
+            self,
+            wheel_diameter: float, axle_track: float,   # both in milimeters
+            left_motor_port: Port = Port.B, right_motor_port: Port = Port.C,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        self.driver = DriveBase(left_motor=Motor(port=left_motor_port,
+                                                 positive_direction=Direction.CLOCKWISE),
+                                right_motor=Motor(port=right_motor_port,
+                                                  positive_direction=Direction.CLOCKWISE),
+                                wheel_diameter=wheel_diameter,
+                                axle_track=axle_track)
+
+        self.ir_sensor = InfraredSensor(ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+    
+    
+    def drive_by_ir_beacon(
+            self,
+            speed: float = 100,     # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+
+        # forward
+        if ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=0)
+
+        # backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=0)
+
+        # turn left on the spot
+        elif ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=-turn_rate)
+
+        # turn right on the spot
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP, Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=turn_rate)
+
+        # turn left forward
+        elif ir_beacon_button_pressed == {Button.LEFT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
+
+        # turn right forward
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=turn_rate)
+
+        # turn left backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
+
+        # turn right backward
+        elif ir_beacon_button_pressed == {Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
+
+        # stop
+        else:
+            self.driver.stop()
 
 
 class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
@@ -34,16 +110,11 @@ class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
             color_sensor_port: Port = Port.S3,
             ir_sensor_port: Port = Port.S4,
             ir_beacon_channel: int = 1):
-        super().__init__()
-
         IRBeaconDriverMixin.__init__(
             self,
-            left_motor_port=left_motor_port,
-            right_motor_port=right_motor_port,
-            ir_sensor_port=ir_sensor_port,
-            ir_beacon_channel=ir_beacon_channel,
-            wheel_diameter=self.WHEEL_DIAMETER,
-            axle_track=self.AXLE_TRACK)
+            wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
         
         self.shooting_motor = Motor(port=shooting_motor_port,
                                     positive_direction=Direction.CLOCKWISE)
