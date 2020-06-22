50685d53fe917ad4d2bceb22ade296f3d36558a6
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-06-22 11:34:53 -0700

    add initial Gripp3r program with Gripp3r.drive_by_ir_beacon

---
+++ official_models/ev3/home/gripp3r/main.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Example LEGO® MINDSTORMS® EV3 Gripp3r Program
+----------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://www.lego.com/en-us/themes/mindstorms/buildarobot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, InfraredSensor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Button, Direction, Port
+
+
+class IRBeaconDriverMixin:
+    """
+    This reusable mixin provides the capability of driving a robot with a Driving Base by the IR beacon
+    """
+    def __init__(
+            self,
+            wheel_diameter: float, axle_track: float,   # both in milimeters
+            left_motor_port: Port = Port.B, right_motor_port: Port = Port.C,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        self.driver = DriveBase(left_motor=Motor(port=left_motor_port,
+                                                 positive_direction=Direction.CLOCKWISE),
+                                right_motor=Motor(port=right_motor_port,
+                                                  positive_direction=Direction.CLOCKWISE),
+                                wheel_diameter=wheel_diameter,
+                                axle_track=axle_track)
+
+        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+    
+    
+    def drive_by_ir_beacon(
+            self,
+            speed: float = 100,     # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+
+        # forward
+        if ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=0)
+
+        # backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=0)
+
+        # turn left on the spot
+        elif ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=-turn_rate)
+
+        # turn right on the spot
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP, Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=turn_rate)
+
+        # turn left forward
+        elif ir_beacon_button_pressed == {Button.LEFT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
+
+        # turn right forward
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=turn_rate)
+
+        # turn left backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
+
+        # turn right backward
+        elif ir_beacon_button_pressed == {Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
+
+        # otherwise stop
+        else:
+            self.driver.stop()
+
+
+class Gripp3r(EV3Brick, IRBeaconDriverMixin):
+    WHEEL_DIAMETER = 26   # milimeters
+    AXLE_TRACK = 102      # milimeters
+
+
+    def __init__(
+            self,
+            left_track_motor_port: Port = Port.B, right_track_motor_port: Port = Port.C,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        IRBeaconDriverMixin.__init__(
+            self,
+            wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
+            left_motor_port=left_track_motor_port, right_motor_port=right_track_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+ 
+    def main(self):
+        """
+        Gripp3r's main program performing various capabilities
+        """
+        while True:
+            self.drive_by_ir_beacon(speed=300)
+
+
+if __name__ == '__main__':
+    GRIPP3R = Gripp3r()
+    GRIPP3R.main()
