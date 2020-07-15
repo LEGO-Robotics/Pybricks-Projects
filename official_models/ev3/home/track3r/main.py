f4101c4efb2d69243844cd3df5cf06366380615f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-14 21:43:14 -0700

    add Track3rWithBlastingBazooka

--- official_models/ev3/home/track3r/main.py
+++ official_models/ev3/home/track3r/main.py
@@ -13,8 +13,9 @@ https://www.lego.com/en-us/themes/mindstorms/buildarobot
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, InfraredSensor
+from pybricks.media.ev3dev import ImageFile, SoundFile
 from pybricks.robotics import DriveBase
-from pybricks.parameters import Button, Direction, Port
+from pybricks.parameters import Button, Direction, Port, Stop
 
 
 class RemoteControlledTank:
@@ -39,7 +40,7 @@ class RemoteControlledTank:
     
     def drive_by_ir_beacon(
             self,
-            speed: float = 100,     # mm/s
+            speed: float = 1000,     # mm/s
             turn_rate: float = 90   # rotational speed deg/s
         ):
         ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
@@ -99,18 +100,21 @@ class RemoteControlledTank:
 
 class Track3r(RemoteControlledTank, EV3Brick):
     WHEEL_DIAMETER = 26   # milimeters
-    AXLE_TRACK = 102      # milimeters
+    AXLE_TRACK = 140      # milimeters
 
 
     def __init__(
             self,
             left_track_motor_port: Port = Port.B, right_track_motor_port: Port = Port.C,
+            medium_motor_port: Port = Port.A,
             ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
         super().__init__(
-            self,
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
             left_motor_port=left_track_motor_port, right_motor_port=right_track_motor_port,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+        self.medium_motor = Motor(port=medium_motor_port,
+                                  positive_direction=Direction.CLOCKWISE)
 
  
     def main(self):
@@ -118,9 +122,42 @@ class Track3r(RemoteControlledTank, EV3Brick):
         Track3r's main program performing various capabilities
         """
         while True:
-            self.drive_by_ir_beacon(speed=300)
+            self.drive_by_ir_beacon(speed=1000)
+
+
+class Track3rWithBlastingBazooka(Track3r):
+    """
+    Track3r blasts his bazooka when the Beacon button is pressed
+    (inspiration from LEGO Mindstorms EV3 Home Edition: Track3r: Tutorial #2)
+    """
+    def blast_bazooka_by_ir_beacon(
+            self, 
+            speed: float = 1000   # degrees per second
+        ):
+        if Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+            self.medium_motor.run_angle(
+                speed=speed,
+                rotation_angle=3 * 360,   # about 3 rotations for 1 shot
+                then=Stop.HOLD,
+                wait=True) 
+
+            self.speaker.play_file(file=SoundFile.LAUGHING_1)
+               
+            while Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                pass
+
+        else:
+            self.medium_motor.stop()
+
+
+    def main(self):
+        self.screen.load_image(ImageFile.PINCHED_MIDDLE)
+            
+        while True:
+            self.drive_by_ir_beacon()
+            self.blast_bazooka_by_ir_beacon()
 
 
 if __name__ == '__main__':
-    TRACK3R = Track3r()
+    TRACK3R = Track3rWithBlastingBazooka()
     TRACK3R.main()
