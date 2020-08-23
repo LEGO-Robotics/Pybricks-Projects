999c24b6e9d0124730d2c056fb473542a8abacfa
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-22 17:33:15 -0700

    add Gripp3r program

--- official_models/ev3/home/gripp3r/main.py
+++ official_models/ev3/home/gripp3r/main.py
@@ -12,9 +12,10 @@ https://www.lego.com/en-us/themes/mindstorms/buildarobot
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, InfraredSensor
+from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
 from pybricks.robotics import DriveBase
-from pybricks.parameters import Button, Direction, Port
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button, Direction, Port, Stop
 from pybricks.tools import wait
 
 
@@ -100,25 +101,67 @@ class RemoteControlledTank:
 
 class Gripp3r(RemoteControlledTank, EV3Brick):
     WHEEL_DIAMETER = 26   # milimeters
-    AXLE_TRACK = 102      # milimeters
+    AXLE_TRACK = 115      # milimeters
 
 
     def __init__(
             self,
             left_track_motor_port: Port = Port.B, right_track_motor_port: Port = Port.C,
+            grip_motor_port: Port = Port.A,
+            touch_sensor_port: Port = Port.S1,
             ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
         super().__init__(
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
             left_motor_port=left_track_motor_port, right_motor_port=right_track_motor_port,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
 
- 
+        self.grip_motor = Motor(port=grip_motor_port,
+                                positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+
+
+    def grip_or_release_by_ir_beacon(self, speed: float = 500):
+        if Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+            if self.touch_sensor.pressed():
+                self.speaker.play_file(file=SoundFile.AIR_RELEASE)
+
+                self.grip_motor.run_time(
+                    speed=speed,
+                    time=1000,
+                    then=Stop.BRAKE,
+                    wait=True)
+
+            else:
+                self.speaker.play_file(file=SoundFile.AIRBRAKE)
+
+                self.grip_motor.run(speed=-speed)
+
+                while not self.touch_sensor.pressed():
+                    pass
+
+                self.grip_motor.stop()
+
+            while Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                pass
+
+
     def main(self):
         """
         Gripp3r's main program performing various capabilities
         """
+        self.grip_motor.run_time(
+            speed=-500,
+            time=1000,
+            then=Stop.BRAKE,
+            wait=True)
+    
         while True:
             self.drive_by_ir_beacon(speed=1000)
+            self.grip_or_release_by_ir_beacon(speed=500)
             wait(1)
 
 
