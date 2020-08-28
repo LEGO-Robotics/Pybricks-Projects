98b95cf637280df63b4febd1e43a4ce6fb092c92
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-08-28 11:10:45 -0700

    refactor Gripp3r program

--- official_models/ev3/home/gripp3r/main.py
+++ official_models/ev3/home/gripp3r/main.py
@@ -4,8 +4,8 @@
 Example LEGO® MINDSTORMS® EV3 Gripp3r Program
 ---------------------------------------------
 
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+This program requires LEGO® EV3 MicroPython v2.0 downloadable at:
+https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
 
 Building instructions can be found at:
 https://www.lego.com/en-us/themes/mindstorms/buildarobot
@@ -21,30 +21,33 @@ from pybricks.tools import wait
 
 class RemoteControlledTank:
     """
-    This reusable mixin provides the capability of driving a robot with a Driving Base by the IR beacon
+    This reusable mixin provides the capability of driving a robot
+    with a Driving Base by the IR beacon
     """
     def __init__(
             self,
             wheel_diameter: float, axle_track: float,   # both in milimeters
             left_motor_port: Port = Port.B, right_motor_port: Port = Port.C,
             ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
-        self.driver = DriveBase(left_motor=Motor(port=left_motor_port,
-                                                 positive_direction=Direction.CLOCKWISE),
-                                right_motor=Motor(port=right_motor_port,
-                                                  positive_direction=Direction.CLOCKWISE),
-                                wheel_diameter=wheel_diameter,
-                                axle_track=axle_track)
+        self.driver = \
+            DriveBase(
+                left_motor=Motor(port=left_motor_port,
+                                 positive_direction=Direction.CLOCKWISE),
+                right_motor=Motor(port=right_motor_port,
+                                  positive_direction=Direction.CLOCKWISE),
+                wheel_diameter=wheel_diameter,
+                axle_track=axle_track)
 
         self.ir_sensor = InfraredSensor(port=ir_sensor_port)
         self.ir_beacon_channel = ir_beacon_channel
-    
-    
+
     def drive_by_ir_beacon(
             self,
             speed: float = 1000,    # mm/s
             turn_rate: float = 90   # rotational speed deg/s
-        ):
-        ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+            ):
+        ir_beacon_button_pressed = \
+            set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
 
         # forward
         if ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
@@ -103,35 +106,39 @@ class Gripp3r(RemoteControlledTank):
     WHEEL_DIAMETER = 26   # milimeters
     AXLE_TRACK = 115      # milimeters
 
-
     def __init__(
             self,
-            left_track_motor_port: Port = Port.B, right_track_motor_port: Port = Port.C,
-            grip_motor_port: Port = Port.A,
+            left_track_motor_port: Port = Port.B,
+            right_track_motor_port: Port = Port.C,
+            gripping_motor_port: Port = Port.A,
             touch_sensor_port: Port = Port.S1,
-            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+            ir_sensor_port: Port = Port.S4,
+            ir_beacon_channel: int = 1):
         super().__init__(
-            wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
-            left_motor_port=left_track_motor_port, right_motor_port=right_track_motor_port,
-            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+            wheel_diameter=self.WHEEL_DIAMETER,
+            axle_track=self.AXLE_TRACK,
+            left_motor_port=left_track_motor_port,
+            right_motor_port=right_track_motor_port,
+            ir_sensor_port=ir_sensor_port,
+            ir_beacon_channel=ir_beacon_channel)
 
         self.ev3_brick = EV3Brick()
 
-        self.grip_motor = Motor(port=grip_motor_port,
-                                positive_direction=Direction.CLOCKWISE)
+        self.gripping_motor = Motor(port=gripping_motor_port,
+                                    positive_direction=Direction.CLOCKWISE)
 
         self.touch_sensor = TouchSensor(port=touch_sensor_port)
 
         self.ir_sensor = InfraredSensor(port=ir_sensor_port)
         self.ir_beacon_channel = ir_beacon_channel
 
-
     def grip_or_release_by_ir_beacon(self, speed: float = 500):
-        if Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+        if Button.BEACON in \
+                self.ir_sensor.buttons(channel=self.ir_beacon_channel):
             if self.touch_sensor.pressed():
                 self.ev3_brick.speaker.play_file(file=SoundFile.AIR_RELEASE)
 
-                self.grip_motor.run_time(
+                self.gripping_motor.run_time(
                     speed=speed,
                     time=1000,
                     then=Stop.BRAKE,
@@ -140,33 +147,36 @@ class Gripp3r(RemoteControlledTank):
             else:
                 self.ev3_brick.speaker.play_file(file=SoundFile.AIRBRAKE)
 
-                self.grip_motor.run(speed=-speed)
+                self.gripping_motor.run(speed=-speed)
 
                 while not self.touch_sensor.pressed():
                     pass
 
-                self.grip_motor.stop()
+                self.gripping_motor.stop()
 
-            while Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+            while Button.BEACON in \
+                    self.ir_sensor.buttons(channel=self.ir_beacon_channel):
                 pass
 
-
-    def main(self):
+    def main(
+            self,
+            driving_speed: float = 1000   # mm/s
+            ):
         """
         Gripp3r's main program performing various capabilities
         """
-        self.grip_motor.run_time(
+        self.gripping_motor.run_time(
             speed=-500,
             time=1000,
             then=Stop.BRAKE,
             wait=True)
-    
+
         while True:
-            self.drive_by_ir_beacon(speed=1000)
+            self.drive_by_ir_beacon(speed=driving_speed)
             self.grip_or_release_by_ir_beacon(speed=500)
             wait(1)
 
 
 if __name__ == '__main__':
     GRIPP3R = Gripp3r()
-    GRIPP3R.main()
+    GRIPP3R.main(driving_speed=1000)
